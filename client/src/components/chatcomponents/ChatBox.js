d63b8ded1f1f05b240b7fb2d725749763fd7662c
Rohanjain96 <rj71829@gmail.com>
2022-07-02 20:54:30 +0530

    make it better

--- client/src/components/chatcomponents/ChatBox.js
+++ client/src/components/chatcomponents/ChatBox.js
@@ -58,7 +58,7 @@ const ChatBox = () => {
             sendmessage()
         }
     }
-    const focus = ()=>setHeight("45vh")
+    const focus = ()=>setHeight("60vh")
     const blur = ()=>setHeight("80vh")
 
     useEffect(() => {       
