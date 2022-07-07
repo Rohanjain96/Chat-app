23ccd31468a9118dfc6b8d49877269d893bf08e8
Rohanjain96 <rj71829@gmail.com>
2022-07-07 14:30:38 +0530

    make it better

--- client/src/components/chatcomponents/ChatBox.js
+++ client/src/components/chatcomponents/ChatBox.js
@@ -58,11 +58,11 @@ const ChatBox = () => {
             sendmessage()
         }
     }
-    const focus = () => setHeight("50vh")
-    const blur = () => setHeight("70vh")
+    const focus = () => setHeight("60vh")
+    const blur = () => setHeight("78vh")
 
     useEffect(() => {
-        setHeight("70vh")
+        setHeight("84vh")
         selectedchatcompare.current = selectedchat;
     }, [])
 
@@ -110,7 +110,7 @@ const ChatBox = () => {
                         width={{ base: "100%", md: "60%" }} height={{base:"100%",lg: "100%" }} position={"relative"} overflow={"hidden"}
                         bg={"gray.200"}>
                         <Box width={"100%"} minHeight={{ base: "7vh", lg: "8vh" }} bg={"gray.100"} display="flex" alignItems={"center"}
-                            pt={2} pb={2} top="0" zIndex={"5"} position={{base:"fixed",lg:"sticky"}}>
+                            pt={2} pb={2} top="0" zIndex={"5"} position="sticky">
                             <IconButton icon={<ArrowBackIcon />} h={9} p="0" d={{ base: "flex", lg: "none" }} size={"md"}
                                 outline={"none"}
                                 onClick={() => setSelectedChat({ type: "changechat", payload: null })} />
@@ -123,14 +123,14 @@ const ChatBox = () => {
                                 </Text>
                             </Box>
                         </Box>
-                        <Box w={"100%"} minHeight={{ base: `${height}`, lg: "86vh" }} mt={{base:"7vh",lg:"0"}} mb={1} zIndex={2} position="relative">
+                        <Box w={"100%"} height={{ base: `${height}`, lg: "86vh" }} mb={1} zIndex={2} position="relative">
                             {
                                 loading ?
                                     <Stack display={"flex"} justifyContent={"center"} alignItems={"center"} width={"100%"} h={"100%"} bg="whitealpha.100" mb={2}>
                                         <Spinner size='xl' />
                                     </Stack>
                                     :
-                                    <Box width={"100%"} h={{ base: "95%", lg: "98%" }} bg="whitealpha.100" mb={2} overflowY={"auto"} p={6} id="Chatbox">
+                                    <Box width={"100%"} h={{ base: "97%", lg: "98%" }} bg="whitealpha.100" mb={2} overflowY={"auto"} p={6} id="Chatbox">
                                         {
                                             fetchedmessages !== undefined && fetchedmessages.map((message, index) => {
                                                 return (
@@ -152,7 +152,7 @@ const ChatBox = () => {
                         <Box display={"flex"} w={"99%"} h={"40px"} alignItems={"center"} position="absolute" bottom={{ base: "2px", lg: "6px" }}>
                             <FormControl display={"flex"} onKeyDown={typinghandler} w={{ base: "95%", lg: "99%" }}>
                                 <Input w={{ base: "100%", lg: "100%" }} autoComplete="disabled" fontSize={"sm"} h={"9"} placeholder=''
-                                    bg="white" ml={{ base: "1", md: "2" }} mr={{ base: "0", lg: "2" }} mt={2} onChange={typinghandler} value={newMessage} onFocus={()=>focus} onBlur={()=>blur} />
+                                    bg="white" ml={{ base: "1", md: "2" }} mr={{ base: "0", lg: "2" }} mt={2} onChange={typinghandler} value={newMessage}  />
                             </FormControl>
                             <IconButton aria-label='Send Message' type='submit' display={{ base: "block", lg: "none" }} w={1} size={"sm"} mt={2}
                                 _focus={{ boxShadow: "none" }} height={"9"} icon={<ArrowForwardIcon />} outline={"none"} onClick={() => {
