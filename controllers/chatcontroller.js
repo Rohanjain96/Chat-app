e72d9d9cd33eaa288408716aafd786e90e231697
Rohanjain96 <rj71829@gmail.com>
2022-09-11 18:49:28 +0530

    make it better

--- controllers/chatcontroller.js
+++ controllers/chatcontroller.js
@@ -1,5 +1,6 @@
 const Chat = require("../models/chatModel");
 const User = require("../models/userModel");
+const CryptoJS = require("crypto-js")
 const accessChat = async (req, res) => {
   const { userId, pic } = req.body;
   if (!userId) {
@@ -49,13 +50,13 @@ const fetchChats = async (req, res) => {
 
     for (let i = 0; i < chats.length; i++) {
       chats[i].notificationcount = 0;
-      // let content = chats[i].latestMessage.content
+      let content = chats[i].latestMessage.content
       
-      // console.log("latest content:",content);
-      // var bytes  = CryptoJS.AES.decrypt(content, 'mysecretkey');
-      // var originalText = bytes.toString(CryptoJS.enc.Utf8);
-      // console.log("orignal text:",originalText);
-      // chats[i].latestMessage.content = originalText;
+      console.log("latest content:",content);
+      var bytes  = CryptoJS.AES.decrypt(content, 'mysecretkey');
+      var originalText = bytes.toString(CryptoJS.enc.Utf8);
+      console.log("orignal text:",originalText);
+      chats[i].latestMessage.content = originalText;
     }
     // console.log("before:",chats)
     // chats = chats.map(chat=>{
