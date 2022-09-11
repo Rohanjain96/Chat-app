d9ee668c6f006166a4769afaba9e58f9af4cffc2
Rohanjain96 <rj71829@gmail.com>
2022-09-11 18:25:58 +0530

    make it better

--- controllers/chatcontroller.js
+++ controllers/chatcontroller.js
@@ -49,13 +49,13 @@ const fetchChats = async (req, res) => {
 
     for (let i = 0; i < chats.length; i++) {
       chats[i].notificationcount = 0;
-      let content = chats[i].latestMessage.content
+      // let content = chats[i].latestMessage.content
       
-      console.log("latest content:",content);
-      var bytes  = CryptoJS.AES.decrypt(content, 'mysecretkey');
-      var originalText = bytes.toString(CryptoJS.enc.Utf8);
-      console.log("orignal text:",originalText);
-      chats[i].latestMessage.content = originalText;
+      // console.log("latest content:",content);
+      // var bytes  = CryptoJS.AES.decrypt(content, 'mysecretkey');
+      // var originalText = bytes.toString(CryptoJS.enc.Utf8);
+      // console.log("orignal text:",originalText);
+      // chats[i].latestMessage.content = originalText;
     }
     // console.log("before:",chats)
     // chats = chats.map(chat=>{
